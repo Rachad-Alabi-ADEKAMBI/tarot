6a8209107fd28cd92e5e06d4e79b5ef7c565e451
Rachad ADEKAMBI <adekambirachad@gmail.com>
2021-09-11 08:16:52 +0000

    Add files via upload

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,25 @@
+let email = document.getElementById("EMAIL");
+let fname = document.getElementById("FNAME");
+
 let form = document.getElementById("form");
 
 function invisible(){
-    form.classList.add("invisible");
+   // form.classList.add("invisible");
+    
+   window.location.replace("index.html");
 }
 
 function visible(){
     form.classList.add("visible");
+
 }
+
+function msg(){
+    console.log("ok");
+}
+
+
+
+
 
 
